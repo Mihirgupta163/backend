a71ab8f49466a66087789b1bc573ee44014507a0
Mihir Gupta <mihirgupta163@gmail.com>
2023-11-15 22:01:21 +0530

    Connection to database is completed

---
+++ src/db/index.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async() => {
+    try {
+        const conn = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+
+        console.log(`Connection is established !! DB_HOST : ${conn.connection.host}`)
+
+    } catch (error) {
+        console.log("MongoDB Connection Error: ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB;
+
+
+
+
+
+
